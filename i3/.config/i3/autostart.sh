5bccb80080ef6068586eb31192fe74e6617f5664
lummax <luogpg@googlemail.com>
2019-01-05 17:17:58 +0100

    i3/autostart: start processes in the background

--- i3/.config/i3/autostart.sh
+++ i3/.config/i3/autostart.sh
@@ -1,28 +1,28 @@
 #! /bin/bash
 
-setxkbmap de neo
-synclient TouchpadOff=1
-feh --bg-scale --no-fehbg ~/.config/i3/background.png
+setxkbmap de neo &
+synclient TouchpadOff=1 &
+feh --bg-scale --no-fehbg ~/.config/i3/background.png &
 
-start_conky_maia
-compton -b
+start_conky_maia &
+compton &
 
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
-pulseaudio
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+pulseaudio &
 
-pa-applet
-nm-applet
-xfce4-power-manager
-pamac-tray
-clipit
-sbxkb
+pa-applet &
+nm-applet &
+xfce4-power-manager &
+pamac-tray &
+clipit &
+sbxkb &
 
-fix_xcursor
+fix_xcursor &
 unclutter &
-xautolock -time 10 -locker blurlock
+xautolock -time 10 -locker blurlock &
 
 if [-d ~/.autostart.d/*.sh ]; then
   for start_snipplet in ~/.autostart.d/*.sh ; do
-      sh $start_snipplet
+      sh $start_snipplet &
   done
 fi
