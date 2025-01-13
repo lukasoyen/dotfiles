b4cc8b246e70042c338e7ca9c668a9045164c5cb
lummax <lummax@mailbox.org>
2025-01-13 16:31:44 +0100

    proper nix installation does not need these file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,9 +3,6 @@ set -gx EDITOR nvim
 fish_add_path --path ~/.local/bin/
 fish_add_path --path ~/.nix-profile/bin/
 
-. '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
-. '/nix/var/nix/profiles/default/etc/profile.d/nix.fish'
-
 direnv hook fish | source
 zoxide init fish --cmd cd | source
 if status is-interactive
