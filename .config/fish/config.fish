deb262e41f6851dfd74861d810c7318bbbfa84f6
lummax <lummax@mailbox.org>
2025-01-13 12:20:02 +0100

    add nix paths

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,8 @@
 set -gx EDITOR nvim
+
+fish_add_path --path ~/.local/bin/
+fish_add_path --path ~/.nix-profile/bin/
+
 direnv hook fish | source
 zoxide init fish --cmd cd | source
 if status is-interactive
