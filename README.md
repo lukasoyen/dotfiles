b6c172dedee147caabe162cf202b9a3706ef65ef
lummax <lummax@mailbox.org>
2025-09-16 16:15:12 +0200

    add install.sh for devcontainer dotfiles

--- README.md
+++ README.md
@@ -9,35 +9,5 @@ Setup
 
 ```
 git clone --recurse-submodules https://github.com/lummax/dotfiles.git && cd dotfiles
-```
-
-### Ubuntu
-
-```
-sudo apt install git curl fontconfig
-curl -fsSL https://install.determinate.systems/nix | sh -s -- install --determinate
-
-nix run .#profile.switch
-
-echo "$(which fish)" | sudo tee -a /etc/shells
-chsh -s $(which fish)
-
-stow --target ~/ .
-
-fc-cache -fv
-```
-
-### MacOS
-
-```
-brew install iterm2 koekeishiya/formulae/skhd
-curl -fsSL https://install.determinate.systems/nix | sh -s -- install --determinate
-
-nix run .#profile.switch
-
-echo "$(which fish)" | sudo tee -a /etc/shells
-chsh -s $(which fish)
-
-stow --target ~/ .
-stow --target ~/ macos
+./install.sh
 ```
---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install iterm2 koekeishiya/formulae/skhd
+    curl -fsSL https://install.determinate.systems/nix | sh -s -- install --determinate —-no-confirm
+
+    . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+    
+    nix run .#profile.switch
+    
+    echo "$(which fish)" | sudo tee -a /etc/shells
+    sudo chsh -s $(which fish) $USER
+    
+    stow --target ~/ .
+    stow --target ~/ macos
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo apt install --yes git curl fontconfig
+    curl -fsSL https://install.determinate.systems/nix | sh -s -- install --determinate —-no-confirm
+
+   . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+
+    nix run .#profile.switch
+    
+    echo "$(which fish)" | sudo tee -a /etc/shells
+    sudo chsh -s $(which fish) $USER
+    
+    stow --target ~/ .
+    
+    fc-cache -fv
+else
+    echo "Unsupported operating system: $OSTYPE"
+    exit 1
+fi
